883965a6b1bc79bb3a3b4fe0807ca6927a5299de
Marcus Vinícius <marcus.vinicius.medeiros@ee.ufcg.edu.br>
2025-09-03 20:31:51 -0300

    Inserindo testes

--- streamlit_app.py
+++ streamlit_app.py
@@ -130,11 +130,20 @@ Dentre as grandezas básicas monitoradas por um sistema deste tipo são:
     if not fases_selecionadas:
         st.warning("Por favor, selecione pelo menos uma fase para exibir o gráfico.")
     else:
+
         # A lógica de filtragem e plotagem é exatamente a mesma de antes
         df_filtrado = df_original[fases_selecionadas]
         
         st.markdown(f"Exibindo dados para: **{', '.join(fases_selecionadas)}**")
-        st.line_chart(df_filtrado)
+
+        st.subheader("Tensões")
+        tab1, tab2 = st.tabs(["Tensão de fase", "Tensão de linha"])
+        with tab1:
+            st.line_chart(df_filtrado)
+        with tab2:
+            st.line_chart(df_filtrado*1.41)
+            
+        st.divider()
         
         with st.expander("Ver tabela de dados completa"):
             st.dataframe(df_original, height=300)
